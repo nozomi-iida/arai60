a0820b67351c68243213626ed91ae20e5cf8c720
nozomi-iida <iida19990106@gmail.com>
2025-04-09 20:47:46 +0900

    setup leetcode

---
+++ algo/src/lib.rs
@@ -0,0 +1,3 @@
+pub mod minimum_spanning_tree;
+pub mod topological_sort;
+pub mod union_find;
---
+++ problems/src/two_sum/step1.rs
@@ -0,0 +1,36 @@
+// Step1
+// 目的: 方法を思いつく
+
+// 方法
+// 5分考えてわからなかったら答えをみる
+// 答えを見て理解したと思ったら全部消して答えを隠して書く
+// 5分筆が止まったらもう一回みて全部消す
+// 正解したら終わり
+
+/*
+  何がわからなかったか
+  -
+
+  何を考えて解いていたか
+  -
+
+  想定ユースケース
+  -
+
+  正解してから気づいたこと
+  -
+*/
+
+pub struct Solution;
+
+/*
+ * @lc app=leetcode id=1 lang=rust
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {}
+}
+// @lc code=end
